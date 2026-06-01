d9e20bc92905dbf3d89ae27691de49dc77fba375
AyushArtesian <ayush.mittal@artesian.io>
2026-06-01 18:49:57 +0530

    feat: Add installation steps for Python 3.11 virtual environment in build script

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+echo "Installing Python 3.11 venv..."
+apt-get update
+apt-get install -y python3.11-venv
+
 echo "Creating virtual environment..."
 python3.11 -m venv /opt/render/project/venv
 
